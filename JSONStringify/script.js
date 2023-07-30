07bd24de51884f13f669d4e53b935141e01263de
Harut20024 <126981162+Harut20024@users.noreply.github.com>
2023-07-30 20:35:01 +0400

    Update JSON.stringify
    this code is update version of JSON stringify

--- JSONStringify/script.js
+++ JSONStringify/script.js
@@ -7,7 +7,7 @@ function stringifyJSON(input) {
   
   // null
   if(input === null) {
-    return "null"; // 
+    return "null"; // change null to "null"
   }
   
   
@@ -15,35 +15,42 @@ function stringifyJSON(input) {
   // number
   if(typeof(input) === "number") {
     if(isNaN(input)) {
-      return "null";
+      return "null"; // change NaN to "Null"
     }
     
-    return "" + input;
+    return "" + input; // change number to "Number"
   }
   
   // boolean
   if(typeof(input) === "boolean") {
-    return "" + input;
+    return "" + input; // change bool to "bool"
   }
   
   // string
   if(typeof(input) === "string") {
-    return "\"" + input + "\"";
+    return "\"" + input + "\""; //adding " " before and after string
   }
   
   
   // array
   if(Array.isArray(input)) {
-    return "[" + input.filter(function(val) {
-      return typeof(val) !== "function";
+    return "[" + input
+    .map(function(val) {//changing undefined to null
+      if(val === undefined) {
+        return null;
+      }
+      return val;//if there is not just continue
+    }).filter(function(val) {
+      return typeof(val) !== "function"; //drop out functions
     }).map(stringifyJSON).join(",") + "]";
   }
   
   // object
   return "{" + Object.keys(input).filter(function(key) {
-    return typeof(input[key]) !== "function";
+    return typeof(input[key]) !== "function" &&
+      input[key] !== undefined;//drop out undefined and functions
   }).map(function(key) {
     const value = input[key];
-    return "\"" + key + "\":" + stringifyJSON(value);
+    return "\"" + key + "\":" + stringifyJSON(value); // adding " "before and after Keys,adding : and adding value
   }) + "}";
 }
