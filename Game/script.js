74e50a1a259a55b12837d8fdbe00292e9efc4e4a
Harut20024 <htarzyanh@gmail.com>
2023-08-09 00:21:48 +0400

    updating game

--- Game/script.js
+++ Game/script.js
@@ -1,176 +1,226 @@
-const numRows = 8;
-const numCols = 6;
-const candyColors = ['red', 'blue', 'green', 'yellow'];
-const gameBoard = document.getElementById('gameBoard');
-let selectedCandy = null;
-let candies = [];
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
 
-function createCandy(row, col) {
-    if (candies[row][col] === null) {
-        return { color: null };
+const BackgroundImg = new Image();
+BackgroundImg.src = "fone.jpg";
+
+const cube = new Image();
+cube.src = "back.png";
+
+
+class GameObj {
+    constructor(x, y, width, height, imgSrc) {
+        this._x = x;
+        this._y = y;
+        this._width = width;
+        this._height = height;
+
+        this._img = new Image();
+        this._img.src = imgSrc;
+        this._img.onload = () => this.render();
     }
-
-    let randomColor;
-    do {
-        randomColor = candyColors[Math.floor(Math.random() * candyColors.length)];
-    } while ((row > 0 && candies[row - 1][col].color === randomColor));
-
-    const candy = {
-        color: randomColor
-    };
-    return candy;
-}
-
-function renderBoard() {
-    gameBoard.innerHTML = '';
-    for (let row = 0; row < numRows; row++) {
-        for (let col = 0; col < numCols; col++) {
-            const candy = candies[row][col];
-            const cell = document.createElement('div');
-            cell.className = 'cell';
-            
-            if (candy.color !== null) {
-                const candyImage = getCandyImageURL(candy.color);
-                cell.style.backgroundImage = `url('${candyImage}')`;
-            }
-            
-            cell.onclick = () => onCandyClick(row, col);
-            gameBoard.appendChild(cell);
-        }
-        gameBoard.appendChild(document.createElement('br'));
+    render() {
+        context.drawImage(this._img, this._x, this._y, this._width, this._height);
     }
 }
 
-function getCandyImageURL(color) {
-    if (color === 'red') {
-        return 'obj-1.png';
-    } else if (color === 'blue') {
-        return 'obj-2.png';
-    } else if (color === 'yellow') {
-        return 'obj-3.png';
-    } else if (color === 'green') {
-        return 'obj-4.png';
+class obj1 extends GameObj {
+    constructor(x, y) {
+        super(x, y, 100, 100, "obj-1.png");
     }
 }
 
-
-
-
-
-function initializeBoard() {
-    candies = []; // Reset the candies array before initializing
-    for (let row = 0; row < numRows; row++) {
-        candies[row] = [];
-        for (let col = 0; col < numCols; col++) {
-            const candy = createCandy(row, col);
-            candies[row][col] = candy;
-        }
-    }
-    
-    // Set first and last cells of the first row to null
-    candies[0][0] = { color: null };
-    candies[3][2] = { color: null };
-    candies[3][3] = { color: null };
-    candies[4][2] = { color: null };
-    candies[4][3] = { color: null };
-    candies[0][numCols - 1] = { color: null };
-    candies[7][0] = { color: null };
-    candies[7][numCols - 1] = { color: null };
-    
-    renderBoard();
-}
-
-function onCandyClick(row, col) {
-    console.log(candies[row][col], row, col);
-    if (selectedCandy === null) {
-        selectedCandy = { row, col };
-    } else {
-        const rowDiff = Math.abs(row - selectedCandy.row);
-        const colDiff = Math.abs(col - selectedCandy.col);
-
-        if (
-            (rowDiff === 1 && colDiff === 0) ||
-            (rowDiff === 0 && colDiff === 1)
-        ) {
-            const targetCandy = candies[row][col];
-            const selectedCandyRow = selectedCandy.row;
-            const selectedCandyCol = selectedCandy.col;
-
-            switch (`${selectedCandyRow},${selectedCandyCol}`) {
-                case '0,0':
-                case '3,3':
-                case '3,2':
-                case '4,3':
-                case '4,2':
-                case `0,${numCols - 1}`:
-                case '7,2':
-                case `7,${numCols - 1}`:
-                    allow = false;
-                    break;
-                default:
-                    allow = true;
-            }
-
-            if (targetCandy.color !== null && allow === true) {
-                // Swap candies
-                const tempColor = targetCandy.color;
-                targetCandy.color = candies[selectedCandyRow][selectedCandyCol].color;
-                candies[selectedCandyRow][selectedCandyCol].color = tempColor;
-
-                // Clear selection
-                selectedCandy = null;
-
-                // Update the game board
-                renderBoard();
-            }
-        } else {
-            // Reset the selection
-            selectedCandy = null;
-        }
+class obj2 extends GameObj {
+    constructor(x, y) {
+        super(x, y, 100, 100, "obj-2.png");
     }
 }
 
-function checkAndDeleteMatches() {
-    for (let row = 0; row < numRows; row++) {
-        for (let col = 0; col <= numCols - 3; col++) {
-            const candyColor = candies[row][col].color;
-
-            // Check horizontal matches
-            if (candyColor && candies[row][col + 1].color === candyColor && candies[row][col + 2].color === candyColor) {
-                candies[row][col].color = null;
-                candies[row][col + 1].color = null;
-                candies[row][col + 2].color = null;
-            }
-        }
+class obj3 extends GameObj {
+    constructor(x, y) {
+        super(x, y, 100, 100, "obj-3.png");
     }
+}
 
+class obj4 extends GameObj {
+    constructor(x, y) {
+        super(x, y, 100, 100, "obj-4.png");
+    }
+}
+
+let data = {
+    objects: []
+};
+
+function update() {
+    // Check for and remove three adjacent obj1 objects
+    // checkAndRemoveThreeAdjacentObjects(obj1);
+    // checkAndRemoveThreeAdjacentObjects(obj2);
+    // checkAndRemoveThreeAdjacentObjects(obj3);
+    // checkAndRemoveThreeAdjacentObjects(obj4);
+
+
+    // Implement your other game logic here
+}
+
+
+function getRandomObjectClass() {
+    const objectClasses = [obj1, obj2, obj3, obj4];
+    const randomIndex = Math.floor(Math.random() * objectClasses.length);
+    return objectClasses[randomIndex];
+}
+
+const objectCoords = [];
+const numRows = 9;
+const numCols = 9;
+const rectSize = 100;
+
+for (let row = 0; row < numRows; row++) {
     for (let col = 0; col < numCols; col++) {
-        for (let row = 0; row <= numRows - 3; row++) {
-            const candyColor = candies[row][col].color;
-
-            // Check vertical matches
-            if (candyColor && candies[row + 1][col].color === candyColor && candies[row + 2][col].color === candyColor) {
-                candies[row][col].color = null;
-                candies[row + 1][col].color = null;
-                candies[row + 2][col].color = null;
-            }
+        const x = col * rectSize + 1000;
+        const y = row * rectSize + 240;
+        if (
+            (x === 1000 && y === 240) ||
+            (x === 1800 && y === 240) ||
+            (x === 1300 && y === 540) ||
+            (x === 1400 && y === 540) ||
+            (x === 1500 && y === 540) ||
+            (x === 1300 && y === 640) ||
+            (x === 1400 && y === 640) ||
+            (x === 1500 && y === 640) ||
+            (x === 1300 && y === 740) ||
+            (x === 1400 && y === 740) ||
+            (x === 1500 && y === 740)
+        ) {
+            objectCoords.push({ x: 3000, y: 3000 });
+        } else {
+            objectCoords.push({ x, y });
         }
     }
 }
 
-function resetGame() {
-    initializeBoard();
+function checkAndRemoveThreeAdjacentObjects(objectType) {
+    const matchedObjects = new Set();
+
+    // Check for horizontal matches
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols - 2; col++) {
+            const obj1A = data.objects[row * numCols + col];
+            const obj1B = data.objects[row * numCols + col + 1];
+            const obj1C = data.objects[row * numCols + col + 2];
+
+            if (
+                obj1A instanceof objectType &&
+                obj1B instanceof objectType &&
+                obj1C instanceof objectType
+            ) {
+                matchedObjects.add(obj1A);
+                matchedObjects.add(obj1B);
+                matchedObjects.add(obj1C);
+            }
+        }
+    }
+
+    // Check for vertical matches
+    for (let col = 0; col < numCols; col++) {
+        for (let row = 0; row < numRows - 2; row++) {
+            const obj1A = data.objects[row * numCols + col];
+            const obj1B = data.objects[(row + 1) * numCols + col];
+            const obj1C = data.objects[(row + 2) * numCols + col];
+
+            if (
+                obj1A instanceof objectType &&
+                obj1B instanceof objectType &&
+                obj1C instanceof objectType
+            ) {
+                matchedObjects.add(obj1A);
+                matchedObjects.add(obj1B);
+                matchedObjects.add(obj1C);
+            }
+        }
+    }
+
+    // Remove matched objects
+    matchedObjects.forEach(obj => {
+        const index = data.objects.indexOf(obj);
+        if (index !== -1) {
+            data.objects.splice(index, 1);
+        }
+    });
+
+    // Call the function recursively if matches were found and removed
+    if (matchedObjects.size > 0) {
+        checkAndRemoveThreeAdjacentObjects(objectType);
+    }
 }
+
+
+
+
+for (const { x, y } of objectCoords) {
+    const ObjectClass = getRandomObjectClass();
+    const newObj = new ObjectClass(x, y);
+    data.objects.push(newObj);
+}
+
+function render() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.drawImage(BackgroundImg, 0, 0, canvas.width, canvas.height);
+
+    for (const { x, y } of objectCoords) {
+        context.drawImage(cube, x, y, rectSize, rectSize);
+    }
+
+    data.objects.forEach(obj => {
+        obj.render();
+    });
+}
+
+let selectedObj = null;
+
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    for (const obj of data.objects) {
+        if (
+            mouseX >= obj._x &&
+            mouseX <= obj._x + obj._width &&
+            mouseY >= obj._y &&
+            mouseY <= obj._y + obj._height
+        ) {
+            if (!selectedObj) {
+                selectedObj = obj;
+            } else if (
+                selectedObj !== obj &&
+                selectedObj._x >= obj._x - 100 &&
+                selectedObj._x <= obj._x + obj._width + 100 &&
+                selectedObj._y >= obj._y - 100 &&
+                selectedObj._y <= obj._y + obj._height + 100
+            ) {
+                // Swap positions of selectedObj and obj
+                const tempX = selectedObj._x;
+                const tempY = selectedObj._y;
+                selectedObj._x = obj._x;
+                selectedObj._y = obj._y;
+                obj._x = tempX;
+                obj._y = tempY;
+                checkAndRemoveMatches()
+                
+                selectedObj = null; // Reset selectedObj
+            }
+            break;
+        }
+    }
+});
 
 function loop() {
-    setTimeout(() => {
-        requestAnimationFrame(loop);
-        checkAndDeleteMatches();
-        renderBoard();
-    }, 800);
+    requestAnimationFrame(loop);
+    update();
+    render();
 }
 
-initializeBoard();
-loop();
-
-
+BackgroundImg.onload = loop;
+cube.onload = loop;
